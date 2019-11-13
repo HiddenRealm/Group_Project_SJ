f615d5efe9029a9851bcade8749538c976768754
HiddenRealm <38515420+HiddenRealm@users.noreply.github.com>
2019-11-13 16:30:11 +0000

    Update prize_gen.py

--- application/prize_gen.py
+++ application/prize_gen.py
@@ -1,3 +1,17 @@
-def Prize_gen():
-	output = "10"
-	return output
+import json
+import boto3
+from boto3.dynamodb.conditions import Key
+import random
+
+def lambda_handler(event, context):
+    randomnum = random.randint(1,101)
+    region='eu-west-1'
+    recList=[]
+    dyndb = boto3.resource('dynamodb', region_name=region)
+    table = dyndb.Table('prizes')
+    response = table.query(
+        KeyConditionExpression=Key('id').eq(randomnum))
+      
+    for i in response['Items']:
+        output = i['prize']
+    return output
