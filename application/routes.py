826e0c21919801fb8e9081065f54116f0d6fe7c5
HiddenRealm <jamestaylor034@gmail.com>
2019-11-19 10:33:29 +0000

    Added a basic pop-up if no info is input, Changed the lambda to the one which sends to the queue

--- application/routes.py
+++ application/routes.py
@@ -8,6 +8,7 @@ import json
 @app.route('/home', methods=['GET', 'POST'])
 def home():
 	form = WinForm()
+	temp3 = ''
 
 	if form.validate_on_submit():
 		random = boto3.client('lambda')
@@ -24,10 +25,19 @@ def home():
 		package = send_data(form.name.data, temp2, form.email.data, 
 				  form.mobile.data, temp)
 
-		random.invoke(FunctionName='PushingData',
+		random.invoke(FunctionName='PushDataToQueue',
 						Payload=json.dumps(package))
 
-	return render_template('home.html', title='Home', form=form)
+		print(package['mobile'])
+
+		if package['email'] == '':
+			print("1")
+			if package['mobile'] == 'None':
+				print("2")
+				temp3 = "You have won " + package['prize'] + " well played."
+				print(temp3)
+
+	return render_template('home.html', title='Home', form=form, prize=temp3)
 
 def send_data(name, acc, ema, mob, prize):
 	output = { "account":acc, "name":name, "email":ema, "mobile":str(mob), "prize":prize}
