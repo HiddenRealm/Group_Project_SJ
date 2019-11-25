852ad4b54e45807eda0be0334b624ed60caa0b39
HiddenRealm <38515420+HiddenRealm@users.noreply.github.com>
2019-11-25 01:35:11 +0000

    Update lambda_load-csv.py

--- application/lambda_load-csv.py
+++ application/lambda_load-csv.py
@@ -7,44 +7,27 @@ def lambda_handler(event, context):
     try:            
         s3=boto3.client('s3')            
         dyndb = boto3.client('dynamodb', region_name=region)
-        confile= s3.get_object(Bucket='testbucket123212321', Key='winners.csv')
+        confile= s3.get_object(Bucket='testbucket123212321', Key='prizes.csv')
         recList = confile['Body'].read().split('\n')
-        firstrecord=True
         csv_reader = csv.reader(recList)
-        
-        for row in csv_reader:
-            
-            try:
-                print(row[1])
-                temp == row[1]
-            except:
+        count = 0
+
+        for row in csv_reader2:
+            if count == 199:
                 break
-            
-            
-            if (firstrecord):
-                firstrecord=False
-                continue
-            
+            count += 1
             
             id = row[0]
-            name = row[1].replace(',','').replace('$','') 
-            account = row[2].replace(',','').replace('$','')
-            email = row[3].replace(',','').replace('$','') if row[3] else 'NULL'
-            mobile_number = row[4].replace(',','').replace('$','') if row[4] else 'NULL'
-            prize = row[5].replace(',','').replace('$','')
-            
-            print(id, name, account, email, mobile_number, prize)
+            prize = row[1]
             
             response = dyndb.put_item(
-                TableName='prizedraw',
+                TableName='prizes',
                 Item={
                 'id' : {'N':str(id)},
-                'name': {'S':name},
-                'account': {'S':str(account)},
-                'email': {'S':str(email)},
-                'mobile number': {'S':str(mobile_number)},
-                'prize': {'S': prize}
+                'prize': {'S':str(prize)}
                 }
             )
+        return "Worked"
+        
     except Exception, e:
         print (str(e))
